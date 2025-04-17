d9ded47968bbbee2a650efdf637c9c562aaec5da
Doniok <okrajnid@gmail.com>
2025-04-17 17:39:51 +0200

    Nadpisanie nazwy użytkownika

--- app/src/main/java/com/example/yoga_app/HomeActivity.kt
+++ app/src/main/java/com/example/yoga_app/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.yoga_app
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.yoga_app.databinding.ActivityHomeBinding
 import com.example.yoga_app.databinding.ActivityLoginBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.snapshots
 
 // Wczytanie elementów interfejsu i bazy danych
 private lateinit var binding: ActivityHomeBinding
@@ -24,12 +27,33 @@ class HomeActivity : AppCompatActivity() {
 
         firestore = FirebaseFirestore.getInstance()
 
-        // pobieranie maila z logowania lub rejestracji
-        val email = intent.getStringExtra("extra_email")
+        // pobieranie maila z logowania lub rejestracji (mail służy jako ID dokumentu)
+        val email = intent.getStringExtra("extra_email").toString()
+
+        //wczytanie przycisku z nazwą użytkownika
+        val btn_username = binding.buttonUsername
+
+
+        // Pobieranie danych z bazy po ID
+        val docRef = firestore.collection("Users").document(email)
+        docRef.get()
+            .addOnSuccessListener {document ->
+                if (document != null) {
+                    Log.d("FIRESTORE","DocumentSnapshot data: ${document.data}")
+
+                    // Nadpisanie tekstu nazwą użytkownika
+                    btn_username.setText(document.getString("username"))
+                } else {
+                    Log.i("FIRESTORE","No such document exist")
+                }
+
+            }
+            .addOnFailureListener {
+               exception -> Log.d("FIRESTORE","get failed with",exception)
+            }
 
 
         // nadpisanie tekstu przycisku na nick użytkownika
-        val btn_username = binding.buttonUsername
-        btn_username.setText(email)
+
     }
 }
